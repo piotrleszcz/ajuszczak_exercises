102111b33734cf3c87165982a8c4fe1f8b8bbc7b
Piotr Leszcz <pleszcz.frontend@gmail.com>
2021-05-11 22:03:34 +0200

    exercise_3

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-var input = prompt ('Podaj swoje imię');
-var length = input.length;
-alert(length);
+// var name = prompt('What is your name?');
+// var age = prompt('How old are you?');
+
+// var data = {
+//   name: name,
+//   age: age,
+// };
+
+// var json = JSON.stringify(data, null, 8);
+
+// alert(json);
+
+var inputName = prompt ("What's your name?");
+var inputAge = prompt ("How old are you?");
+
+var person = {
+    name: inputName,
+    age: inputAge
+}
+
+console.log(person);
